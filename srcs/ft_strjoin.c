c78ca3cde57fb76ed2a54243a00d321f13fc5f4d
anremiki <feyral@hotmail.fr>
2022-04-02 23:07:30 +0200

    lib update

--- srcs/ft_strjoin.c
+++ srcs/ft_strjoin.c
@@ -6,11 +6,11 @@
 /*   By: anremiki <anremiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 12:39:26 by anremiki          #+#    #+#             */
-/*   Updated: 2021/10/14 00:24:53 by anremiki         ###   ########.fr       */
+/*   Updated: 2022/03/26 15:35:17 by anremiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "../includes/libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
